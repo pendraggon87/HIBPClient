598275241f4810e4c5acd441f9cd24b1763071b4
Aaron Katz <pendraggon87@gmail.com>
2018-05-13 15:27:49 -0400

    Add project files.

---
+++ HIBPClient/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIBPClient
+{
+    public class Class1
+    {
+    }
+}
